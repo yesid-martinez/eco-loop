53c88265fe40e3e32cfc00337a40204f3c87b7b8
Yesid M <wujibifan.hh@gmail.com>
2024-11-13 03:30:20 -0500

    initialize React application with Vite
    create basic structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="textContainer">
+        <h1>Hello EcoLoop!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
